ea1d837a3af08078b1388d866c6bdfe5e1da9aee
JorgeDavidRivas <135989979+JorgeDavidRivas@users.noreply.github.com>
2023-06-15 09:38:04 -0500

    Add files via upload

---
+++ Programa.py
@@ -0,0 +1,8 @@
+from matematicas import sumar, restar, division, producto
+from algebra import distancia
+
+print(sumar(2,3))
+print(restar(4, 2))
+print(division(8, 4))
+print(producto(4,3))
+print(distancia(1, 2, 3, 4))
